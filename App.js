e55275123b207ee345d1fe39aaf62066116a0625
Haren <harensharma33@gmail.com>
2023-05-06 22:02:11 +0530

    Search Destinations

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import {View, Text, SafeAreaView} from 'react-native';
 import HomeScreen from './src/screens/Home';
 import Post from './src/components/Post';
 import SearchResultsScreen from './src/screens/SearchResult';
+import DestinationSearchScreen from './src/screens/DestinationSearchScreen';
 
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      {/* <HomeScreen /> */}
-      <SearchResultsScreen />
+      <DestinationSearchScreen/>
     </SafeAreaView>
   );
 };
