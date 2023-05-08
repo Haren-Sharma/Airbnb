2a619c66b8ff8b84f96199188f189ff6b7efbdc2
Haren <harensharma33@gmail.com>
2023-05-08 15:08:32 +0530

    init guest screen

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import HomeScreen from './src/screens/Home';
 import Post from './src/components/Post';
 import SearchResultsScreen from './src/screens/SearchResult';
 import DestinationSearchScreen from './src/screens/DestinationSearchScreen';
+import GuestScreen from './src/screens/Guests';
 
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <DestinationSearchScreen/>
+      <GuestScreen/>
     </SafeAreaView>
   );
 };
